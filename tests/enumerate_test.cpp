96dd30fe3565b7cf7febe6045881d32f4c09b1b5
lg <lg4869@outlook.com>
2018-03-04 15:47:14 +0800

    adopt range in utility

--- tests/enumerate_test.cpp
+++ tests/enumerate_test.cpp
@@ -2,11 +2,11 @@
 #include <cstdio>
 
 #include <crystalnet/utility/enumerate.hpp>
-#include <teavana/range.hpp>
+#include <crystalnet/utility/range.hpp>
 
 void test_1()
 {
-    for (auto[idx, n] : enumerate(tea::range(100, 110))) {
+    for (auto[idx, n] : enumerate(range(100, 110))) {
         assert(idx + 100 == n);
         printf("%d %d\n", idx, n);
     }
