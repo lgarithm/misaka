ed0937ba576f3c805f105fb72ae7cd7966843226
lg <lg4869@outlook.com>
2018-03-10 18:11:31 +0800

    use function object instead of function pointer

--- src/crystalnet/core/operator.hpp
+++ src/crystalnet/core/operator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <memory>
 #include <string>
 #include <type_traits>
 
@@ -31,19 +32,34 @@ struct backward_ctx_t {
     }
 };
 
+struct shape_func_t {
+    virtual shape_t operator()(const shape_list_t &) = 0;
+    virtual ~shape_func_t() {}
+};
+
+struct forward_func_t {
+    virtual void operator()(const forward_ctx_t &) = 0;
+    virtual ~forward_func_t() {}
+};
+
+struct backward_func_t {
+    virtual void operator()(const backward_ctx_t &) = 0;
+    virtual ~backward_func_t() {}
+};
+
 struct operator_t {
     const uint8_t arity;
-    operator_t(const char *const name, uint8_t arity, shape_func_t infer,
-               forward_func_t eval, backward_func_t feed)
+    operator_t(const char *const name, uint8_t arity, shape_func_t *infer,
+               forward_func_t *eval, backward_func_t *feed)
         : name(name), arity(arity), infer(infer), forward(eval), backward(feed)
     {
     }
 
     const std::string name;
 
-    shape_func_t *infer;
-    forward_func_t *forward;
-    backward_func_t *backward;
+    std::unique_ptr<shape_func_t> infer;
+    std::unique_ptr<forward_func_t> forward;
+    std::unique_ptr<backward_func_t> backward;
 };
 
 struct initializer_t {
@@ -51,22 +67,43 @@ struct initializer_t {
     virtual ~initializer_t() {}
 };
 
-template <typename T> struct operator_creator_t {
-    static void forward(forward_ctx_t *ctx) { (*(typename T::forward *)ctx)(); }
-    static void backward(backward_ctx_t *ctx)
+struct simple_shape_func_t : shape_func_t {
+    typedef shape_t *(shape_func_ptr_t)(const shape_list_t *);
+    shape_func_ptr_t *fn_ptr;
+    simple_shape_func_t(shape_func_ptr_t *fn_ptr) : fn_ptr(fn_ptr) {}
+    shape_t operator()(const shape_list_t &shape_list) override
     {
-        (*(typename T::backward *)ctx)();
+        shape_t *p_shape = fn_ptr(&shape_list);
+        shape_t shape(*p_shape);
+        free_shape(p_shape);
+        return shape;
+    }
+};
+
+template <typename T, typename S> void call(S &ctx)
+{
+    static_assert(std::is_base_of<S, T>::value);
+    (*(T *)&ctx)();
+}
+
+template <typename T> struct simple_forward_func_t : forward_func_t {
+    void operator()(const forward_ctx_t &ctx) override
+    {
+        call<typename T::forward>(ctx);
+    }
+};
+
+template <typename T> struct simple_backward_func_t : backward_func_t {
+    void operator()(const backward_ctx_t &ctx) override
+    {
+        call<typename T::backward>(ctx);
     }
 };
 
 template <typename T> operator_t *_register_bi_op(const char *const name)
 {
-    return register_op(name, T::arity, T::infer, operator_creator_t<T>::forward,
-                       operator_creator_t<T>::backward);
-}
-
-template <typename T, typename S> void call(S &op)
-{
-    static_assert(std::is_base_of<S, T>::value);
-    (*(T *)&op)();
+    shape_func_t *infer = new simple_shape_func_t(T::infer);
+    forward_func_t *eval = new simple_forward_func_t<T>;
+    backward_func_t *feed = new simple_backward_func_t<T>;
+    return register_op(name, T::arity, infer, eval, feed);
 }
