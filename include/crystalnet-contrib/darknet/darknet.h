28f913a71d972eff17ba781a3a965f0686c254fa
lg <lg4869@outlook.com>
2018-06-01 22:07:52 +0800

    fix region layer

--- include/crystalnet-contrib/darknet/darknet.h
+++ include/crystalnet-contrib/darknet/darknet.h
@@ -7,8 +7,6 @@ extern "C" {
 
 extern void reorg_cpu(float *x, int w, int h, int c, int batch, int stride,
                       int forward, float *out);
-extern void forward_region_layer(float *input, int batch, float *output,
-                                 int outputs, int _n, int w, int h);
 
 #ifdef __cplusplus
 }
