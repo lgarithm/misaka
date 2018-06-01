f7560084d43edf87b20aebeff077d70ec55060d8
lg <lg4869@outlook.com>
2018-06-01 09:51:57 +0800

    fix im2col

--- src/crystalnet-contrib/darknet/darknet.cpp
+++ src/crystalnet-contrib/darknet/darknet.cpp
@@ -7,13 +7,7 @@
 #include <crystalnet/core/tensor.hpp>
 #include <crystalnet/core/tracer.hpp>
 
-std::string summary(float *x, int n)
-{
-    tensor_ref_t _r(shape_t(n), dtypes.f32, x);
-    r_tensor_ref_t<float> r(_r);
-    return summary(r);
-}
-
+// the original implementation, which is likely wrong.
 void reorg_cpu(float *x, int w, int h, int c, int batch, int stride,
                int forward, float *out)
 {
@@ -36,43 +30,6 @@ void reorg_cpu(float *x, int w, int h, int c, int batch, int stride,
                     else
                         out[in_index] = x[out_index];
                 }
-            }
-        }
-    }
-}
-
-float im2col_get_pixel(float *im, int height, int width, int channels, int row,
-                       int col, int channel, int pad)
-{
-    row -= pad;
-    col -= pad;
-
-    if (row < 0 || col < 0 || row >= height || col >= width) return 0;
-    return im[col + width * (row + height * channel)];
-}
-
-// From Berkeley Vision's Caffe!
-// https://github.com/BVLC/caffe/blob/master/LICENSE
-void im2col_cpu(float *data_im, int channels, int height, int width, int ksize,
-                int stride, int pad, float *data_col)
-{
-    int c, h, w;
-    int height_col = (height + 2 * pad - ksize) / stride + 1;
-    int width_col = (width + 2 * pad - ksize) / stride + 1;
-
-    int channels_col = channels * ksize * ksize;
-    for (c = 0; c < channels_col; ++c) {
-        int w_offset = c % ksize;
-        int h_offset = (c / ksize) % ksize;
-        int c_im = c / ksize / ksize;
-        for (h = 0; h < height_col; ++h) {
-            for (w = 0; w < width_col; ++w) {
-                int im_row = h_offset + h * stride;
-                int im_col = w_offset + w * stride;
-                int col_index = (c * height_col + h) * width_col + w;
-                data_col[col_index] =
-                    im2col_get_pixel(data_im, height, width, channels, im_row,
-                                     im_col, c_im, pad);
             }
         }
     }
